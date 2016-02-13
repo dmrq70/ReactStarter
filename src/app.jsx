7a054e1edc1b8afa4df0f6fd6587dd5080cfca1c
Diego Reyes <diego@motorcolectivo.com.ar>
2016-02-13 17:50:06 -0300

    Conectado a firebase

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,20 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var ReactFire = require( 'reactfire');
+var Firebase = require( 'firebase');
+var rootUrl = 'https://udemyreactapp.firebaseio.com/';
 
 var Hello = React.createClass({
+  mixins: [ ReactFire ],
+  componentWillMount: function () {
+    this.bindAsObject( new Firebase( rootUrl + 'items/' ), 'items' );
+    // en este punto this.state.items es un objeto que contiene lo que
+    // el objeto 'items' tiene en la base en firebase
+  },
   render: function() {
+    console.log( this.state );
     return <h1 className="red">
-      Hello!
+      Hola!
     </h1>
   }
 });
